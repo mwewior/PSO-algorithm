d9ed0d5dd920d85299c66244ab83e9169a758815
mwewior <01168918@pw.edu.pl>
2023-12-30 00:06:37 +0100

    printing

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import time
 def f(x, fun):
     if fun == 1:
         return sum(xi ** 2 for xi in x)
-    elif fun == 2:  # booth
+    elif fun == 2:   # booth
         return (x[0] + 2*x[1] - 7)**2 + (2*x[0] + x[1] - 5)**2
-    elif fun == 3:  # rosenbrock
+    elif fun == 3:   # rosenbrock
         return (100.0 * (x[1] - x[0]**2)**2 + (1 - x[0])**2)
-    elif fun == 4:  # ackley
+    elif fun == 4:   # ackley
         n = len(x)
         sum1 = sum(xi**2 for xi in x)
         sum2 = sum(np.cos(2.0 * np.pi * xi) for xi in x)
@@ -19,7 +19,7 @@ def f(x, fun):
     elif fun == 5:  # michalewicz
         m = 10
         return -np.sin(x[0]) * np.sin(x[0]**2 / np.pi)**(2 * m) - np.sin(x[1]) * np.sin(2 * x[1]**2 / np.pi)**(2 * m)
-    elif fun == 6:  # holder_table
+    elif fun == 6:   # holder_table
         return -abs(np.sin(x[0]) * np.cos(x[1]) * np.exp(abs(1 - np.sqrt(x[0]**2 + x[1]**2)/np.pi)))
 
 
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     # Parametry algorytmu PSO
     dimensions = 2
     num_particles = 25
-    max_iterations = 150
+    max_iterations = 250
     initial_inertia_weight = 0.72984
     c1 = 2.05
     c2 = 2.05
@@ -54,18 +54,18 @@ if __name__ == '__main__':
     fun = 6 # wybór funkcji :
             # 1 - wielomianowa  - f(x) = 0 x = (0, 0)
             # 2 - booth - f(x) = 0 x = (1, 3)
-            # 3 - rosenbrock - f(x) = 0 x = (1, 1)
-            # 4 - ackley  - f(x) = 0 x = (0, 0)
-            # 5 - michalewicz - f(x) = -1.8013 x = (2.20, 1.57)
+            # 3 - rosenbrock - f(x) = 0 x = (0, 0)
+            # 4 - ackley  - f(x) = -1.8013 x = (2.20, 1.57)
+            # 5 - michalewicz - f(x) = 0 x = (0, 0)
             # 6 - holder_table - f(x) = -19.2085 x = (8.05502, 9.66459), x = (-8.05502, 9.66459), x = (8.05502, -9.66459), x = (-8.05502, -9.66459)
     min_bound, max_bound = bounds(fun)
 
-    draw_online = 0  # czy rysować online
-    draw_result = 1  # czy narysować wynik
+    draw_online = 0   # czy rysować online
+    draw_result = 0   # czy narysować wynik
 
     inertia_mode = 1
 
-    num_tests = 200
+    num_tests = 1000
     best_fitnesses = []
 
     for _ in range(num_tests):
@@ -82,8 +82,10 @@ if __name__ == '__main__':
     if draw_result:
         plot.draw_result(best_position, best_fitness, min_bound, max_bound, fun)
 
-    trash_num = int(0.05*num_tests)
+    trash_num = int(0.05 * num_tests)
     average = sum(sorted(best_fitnesses)[trash_num:-trash_num])/(num_tests - trash_num)
+
+    average2 = sum(sorted(best_fitnesses)) / num_tests
     # print(sorted(best_fitnesses))
 
     print(f'Average best fitness = {average} for mode {inertia_mode}')
