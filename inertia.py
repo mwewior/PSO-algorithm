17b932c02d233b79be5d868afce27a82be054096
Natalia <natalia@pop-os.localdomain>
2023-12-29 16:18:56 +0100

    poprawka

--- inertia.py
+++ inertia.py
@@ -7,7 +7,7 @@ def mode2(i, max_iterations, initial_inertia_weight):
 
 def mode3(particle_best_fitness, global_best_fitness, initial_inertia_weight):
 ## Współczynnik bezwładności zależny od jakości rozwiązania.
-    dif = particle_best_fitness - global_best_fitness
+    dif = abs(particle_best_fitness - global_best_fitness)
 
     inertia_weight = (initial_inertia_weight+0.1)*(dif/(dif+1))
     return inertia_weight
