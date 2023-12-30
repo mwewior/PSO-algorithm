ec55635c6e2ba50fa62ace0a098115bcf89e944d
mwewior <01168918@pw.edu.pl>
2023-12-30 10:54:08 +0100

    minor refactor

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-import pso
-import plot
-import numpy as np
 import time
 import yaml
+import numpy as np
 
-
-def f(x, fun):
-    if fun == 1:
-        return sum(xi ** 2 for xi in x)
-    elif fun == 2:   # booth
-        return (x[0] + 2*x[1] - 7)**2 + (2*x[0] + x[1] - 5)**2
-    elif fun == 3:   # rosenbrock
-        return (100.0 * (x[1] - x[0]**2)**2 + (1 - x[0])**2)
-    elif fun == 4:   # ackley
-        n = len(x)
-        sum1 = sum(xi**2 for xi in x)
-        sum2 = sum(np.cos(2.0 * np.pi * xi) for xi in x)
-        return -20.0 * np.exp(-0.2 * np.sqrt(sum1 / n)) - np.exp(sum2 / n) + 20 + np.exp(1)
-    elif fun == 5:  # michalewicz
-        m = 10
-        return -np.sin(x[0]) * np.sin(x[0]**2 / np.pi)**(2 * m) - np.sin(x[1]) * np.sin(2 * x[1]**2 / np.pi)**(2 * m)
-    elif fun == 6:   # holder_table
-        return -abs(np.sin(x[0]) * np.cos(x[1]) * np.exp(abs(1 - np.sqrt(x[0]**2 + x[1]**2)/np.pi)))
-
-
-def bounds(fun):
-    if fun == 1:
-        return -5, 5
-    elif fun == 2:  # booth
-        return -10, 10
-    elif fun == 3:  # rosenbrock
-        return -5, 10
-    elif fun == 4:  # ackley
-        return -32.768, 32.768
-    elif fun == 5:  # michalewicz
-        return 0, np.pi
-    elif fun == 6:  # holder_table
-        return -10, 10
+import pso
+import plot
+import functions
 
 
 if __name__ == '__main__':
@@ -51,7 +19,7 @@ if __name__ == '__main__':
     num_tests    =  params['num_tests']
     fun          =  params['fun']
 
-    min_bound, max_bound = bounds(fun)
+    min_bound, max_bound = functions.bounds(fun)
 
     best_fitnesses = []
 
@@ -68,17 +36,15 @@ if __name__ == '__main__':
 
     end_time = time.time()
     elapsed_time = end_time - start_time
+
     print(f'algorithm took {elapsed_time} seconds')
-
     print(f'The test was run {num_tests} times')
-
-    if draw_result:
-        plot.draw_result(best_position, best_fitness, min_bound, max_bound, fun)
 
     trash_num = int(0.05 * num_tests)
     average = sum(sorted(best_fitnesses)[trash_num:-trash_num])/(num_tests - trash_num)
-
     average2 = sum(sorted(best_fitnesses)) / num_tests
-    # print(sorted(best_fitnesses))
 
     print(f'Average best fitness = {average} for mode {inertia_mode}\n\t\t\t{average2}')
+
+    if draw_result:
+        plot.draw_result(best_position, best_fitness, min_bound, max_bound, fun)
--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import main
+import functions
 
 
 def draw_grid(min_bound, max_bound, fun):
@@ -8,7 +8,7 @@ def draw_grid(min_bound, max_bound, fun):
     step = abs(max_bound - min_bound)/1000
     X, Y = np.meshgrid(np.arange(min_bound,max_bound, step) , np.arange(min_bound,max_bound, step) )
 
-    Z = main.f([X, Y], fun)
+    Z = functions.f([X, Y], fun)
 
     fig = plt.figure()
 
@@ -20,7 +20,6 @@ def draw_grid(min_bound, max_bound, fun):
 
     plt.xlabel('X')
     plt.ylabel('Y')
-
 
 
 def draw_online(min_bound, max_bound, fun):
@@ -37,15 +36,19 @@ def draw_online(min_bound, max_bound, fun):
 
 
 def draw_result(best_position, best_fitness, min_bound, max_bound, fun):
-    
+
     draw_grid(min_bound, max_bound, fun)
     # Oznaczenie znalezionego punktu
     plt.scatter([best_position[0]], [best_position[1]], color='w', s=50, label='Best Position')
-
     plt.show()
+    # plt.show(block=False)
+    # plt.pause(0.01)
+    # return
+
 
 def off():
     plt.ioff()
+
 
 def pause():
     plt.pause(0.1)
