90e29209713c7831a9c6be0f408f88d1e88826a6
mwewior <01168918@pw.edu.pl>
2023-12-30 18:49:38 +0100

    dodawanie wyników testów do jsona, jeszcze nie uwzględnione nasze funkcje

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,10 @@
+import os
 import json
 
+import functions as f
+
+
+fun_amount = 6
 
 """
 {
@@ -33,7 +38,6 @@ import json
 
                     ...
 
-
                 ]
             },
 
@@ -47,10 +51,69 @@ import json
 """
 
 
-def edit_file(file_path, test_result_dict):
+def read_and_edit_file(file_path):
+    if os.path.getsize(file_path) <= 1:
+        size = 0
+        data = {
+            "benchmark": {
+                "tests": []
+            }
+        }
+        with open(file_path, 'w') as json_file:
+            json.dump(data, json_file, indent=2)
+
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
 
+
+    file_content = data["benchmark"]["tests"]
+    print(file_content)
+    size = len(file_content)
+
+    common = [] # tu trzeba będzie wklepać parametry ogólne
+
+
+    new_content = {
+        "test_id": size+1,
+        "common parameters": common,
+        "results": []
+    }
+
+    results = new_content["results"]
+
+    for f_id in range(fun_amount):
+
+        cur_result = {
+            "f_id": f_id+1,
+            "function_type": f.names(f_id+1),
+            "inertia": []
+        }
+
+        for mode in range(3):
+            values = [1]
+            times = [8]
+
+            cur_test = {
+                "mode": mode,
+                "values": values,
+                "time": times,
+            }
+            cur_result["inertia"].append(cur_test)
+        results.append(cur_result)
+        print(new_content)
+    file_content.append(new_content)
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+read_and_edit_file('./results/param_json.json')
+
+
+# test_num = len(o)
+# read_file('./results/param_json.json')
+
+# def edit_file(file):
+#     size = len(file)
+#     new_data =
 
 
 
